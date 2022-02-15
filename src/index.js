67c4327bcec2bdde0c6df42bcb44e986cc06e994
Pedro Silva <pedrosilva_02@outlook.com>
2022-02-15 22:57:38 +0000

    Remove unnecessary dependencies and added country

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+//Using js insted of ts because c.socket doesn't "exist" in @types/telnetlib
+
 const telnetlib = require('telnetlib')
+var geoip = require('geoip-country');
 const fs = require('fs')
 
 var access = fs.createWriteStream('./access.log', { flags: 'a' })
 
 const server = telnetlib.createServer({ }, (c) => {
   c.on('negotiated', () => {
-    console.log(c.socket.remoteAddress)
-    access.write(c.socket.remoteAddress + "\r\n");
+    var ip = c.socket.remoteAddress
+
+    if (ip.startsWith('::ffff:'))
+      ip = ip.split(':')[3]
     c.end()
+
+    const ct = geoip.lookup(c.socket.remoteAddress).country;
+    access.write(ip + " - " + ct + "\r\n");
+    console.log(ip + " - " + ct);
   });
 });
 server.listen(23);
